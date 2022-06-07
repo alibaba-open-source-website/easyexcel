c973893843c4257d820da1c5b265d97b51231c3b
Jiaju Zhuang <zhuangjiaju@qq.com>
2022-06-07 11:38:39 +0800

    优化seo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -101,7 +101,7 @@ const config = {
                 },
                 {
                     name: 'description',
-                    content: 'EasyExcel是一个基于Java的，快速、简洁、解决大文件内存溢出的Excel处理工具。他能让你在不用考虑性能、内存的等因素的情况下，快速完成Excel的读、写等功能。'
+                    content: 'EasyExcel是一个基于Java的、快速、简洁、解决大文件内存溢出的Excel处理工具。他能让你在不用考虑性能、内存的等因素的情况下，快速完成Excel的读、写等功能。'
                 },
             ],
             // 最上面的广告位  https://docusaurus.io/docs/api/themes/configuration#announcement-bar

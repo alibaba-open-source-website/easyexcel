19529f18f15544f5725b729d025bf1bb211c5638
Jiaju Zhuang <zhuangjiaju@qq.com>
2022-06-27 21:49:01 +0800

    再来一个广告

--- src/theme/MDXContent/index.tsx
+++ src/theme/MDXContent/index.tsx
@@ -8,14 +8,13 @@
 import React from 'react';
 import MDXContent from '@docusaurus/theme-classic/lib/theme/MDXContent';
 import type {Props} from '@theme/MDXContent';
-import styles from './styles.module.css';
 import classnames from 'classnames'
 
 export default function MDXContentWrapper({children}: Props): JSX.Element {
     return (
         <>
             {/* 在加载markdown的时候加入个广告 */}
-            <div className={classnames('wwads-cn', 'wwads-horizontal', styles.advertising)} data-id="173"></div>
+            <div className={classnames('wwads-cn', 'wwads-horizontal')} data-id="173"></div>
             <MDXContent {...{children}} />
         </>
     );

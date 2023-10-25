0e46c5152f2670a5189d23171c2af001f978ca36
Jerry Fan <huanyueyaoqin@qq.com>
2023-10-25 15:43:20 +0800

    chore: 增加了Chat2DB链接在首页

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,95 +16,105 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
 const features = [
-    {
-        title: '快速',
-        imageUrl: 'img/undraw_docusaurus_mountain.svg',
-        description: (
-            <>
-                快速的读取<code>excel</code>中的数据。
-            </>
-        ),
-    },
-    {
-        title: '简洁',
-        imageUrl: 'img/undraw_docusaurus_tree.svg',
-        description: (
-            <>
-                映射<code>excel</code>和实体类，让代码变的更加简洁。
-            </>
-        ),
-    },
-    {
-        title: '大文件',
-        imageUrl: 'img/undraw_docusaurus_react.svg',
-        description: (
-            <>
-                在读写大文件的时候使用磁盘做缓存，更加的节约内存。
-            </>
-        ),
-    },
+  {
+    title: '快速',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        快速的读取<code>excel</code>中的数据。
+      </>
+    ),
+  },
+  {
+    title: '简洁',
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    description: (
+      <>
+        映射<code>excel</code>和实体类，让代码变的更加简洁。
+      </>
+    ),
+  },
+  {
+    title: '大文件',
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: <>在读写大文件的时候使用磁盘做缓存，更加的节约内存。</>,
+  },
 ];
 
 function Feature({imageUrl, title, description}) {
-    const imgUrl = useBaseUrl(imageUrl);
-    return (
-        <div className={clsx('col col--4', styles.feature)}>
-            {imgUrl && (
-                <div className="text--center">
-                    <img className={styles.featureImage} src={imgUrl} alt={title}/>
-                </div>
-            )}
-            <h3>{title}</h3>
-            <p>{description}</p>
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
-    );
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 }
 
 export default function Home() {
-    const context = useDocusaurusContext();
-    const {siteConfig = {}} = context;
-    return (
-        <Layout
-            // 首页的标题
-            title="EasyExcel官方文档 - 基于Java的Excel处理工具"
-            // 首页的description
-            description={siteConfig.tagline}>
-            <header className={clsx('hero hero--primary', styles.heroBanner)}>
-                <div className="container">
-                    <h1 className="hero__title">{siteConfig.title}</h1>
-                    <p className="hero__subtitle">
-                        <span>EasyExcel是一个基于Java的、快速、简洁、解决大文件内存溢出的Excel处理工具。</span><br/><span>他能让你在不用考虑性能、内存的等因素的情况下，快速完成Excel的读、写等功能。</span>
-                    </p>
-                    <div className={styles.buttons}>
-                        <Link
-                            className={clsx(
-                                'button button--outline button--secondary button--lg',
-                                styles.getStarted,
-                            )}
-                            to={useBaseUrl('/docs/current/')}>
-                            Get Started
-                        </Link>
-                    </div>
-                </div>
-            </header>
-            <main>
-                {features && features.length > 0 && (
-                    <section className={styles.features}>
-                        <div className="container">
-                            <div className="row">
-                                {features.map(({title, imageUrl, description}) => (
-                                    <Feature
-                                        key={title}
-                                        title={title}
-                                        imageUrl={imageUrl}
-                                        description={description}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    </section>
-                )}
-            </main>
-        </Layout>
-    );
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      // 首页的标题
+      title="EasyExcel官方文档 - 基于Java的Excel处理工具"
+      // 首页的description
+      description={siteConfig.tagline}>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">
+            <span>
+              EasyExcel是一个基于Java的、快速、简洁、解决大文件内存溢出的Excel处理工具。
+            </span>
+            <br />
+            <span>
+              他能让你在不用考虑性能、内存的等因素的情况下，快速完成Excel的读、写等功能。
+            </span>
+
+            <div>
+              <Link to={'https://www.sqlgpt.cn'} className={styles.chat2dbBtn}>
+                🥳 Chat2DB 🥳
+                <div style={{fontSize: "18px"}}>一个AI智能的数据库管理工具</div>
+              </Link>
+            </div>
+          </p>
+
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('/docs/current/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map(({title, imageUrl, description}) => (
+                  <Feature
+                    key={title}
+                    title={title}
+                    imageUrl={imageUrl}
+                    description={description}
+                  />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
 }
